de9e71777aed6a409762cf1b202aba14b0124ccb
amberlai1 <alai8@u.rochester.edu>
2022-08-22 12:40:23 -0400

    imported train tsv

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import pandas as pd
 
 url = "https://raw.githubusercontent.com/Rochester-Biomedical-DS/Hackathon-Summer-2022/main/prediction/prediction.csv"
 download = pd.read_csv('https://raw.githubusercontent.com/Rochester-Biomedical-DS/Hackathon-Summer-2022/main/test_data/features_test.tsv', sep='\t')
-
-print(download.head)
+train = pd.read_csv('https://raw.githubusercontent.com/Rochester-Biomedical-DS/Hackathon-Summer-2022/main/train_data/features_train.tsv', sep ='\t')
+print(download.columns)
+print (train.head)
