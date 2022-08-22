cb1e9deddc0e29d1633654a4d2f0d3ca04c6869a
amberlai1 <alai8@u.rochester.edu>
2022-08-22 12:23:01 -0400

    test tsv file imported

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import pandas as pd
-import requests
-import io
+
 
 url = "https://raw.githubusercontent.com/Rochester-Biomedical-DS/Hackathon-Summer-2022/main/prediction/prediction.csv"
-download = requests.get(url).content
+download = pd.read_csv('https://raw.githubusercontent.com/Rochester-Biomedical-DS/Hackathon-Summer-2022/main/test_data/features_test.tsv', sep='\t')
 
-df = pd.read_csv(io.StringIO(download.decode('utf-8')))
-
-print(df.head)
+print(download.head)
